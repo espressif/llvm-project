36cf87e1745c842a45459638984a31d34cccecea
Andrei Safronov <safronov@espressif.com>
2024-08-14 20:42:06 +0300

    [Xtensa] Implement support for the BranchRelaxation

--- llvm/lib/Target/Xtensa/XtensaTargetMachine.cpp
+++ llvm/lib/Target/Xtensa/XtensaTargetMachine.cpp
@@ -102,6 +102,7 @@ public:
   }
 
   bool addInstSelector() override;
+  void addPreEmitPass() override;
 };
 } // end anonymous namespace
 
@@ -109,6 +110,8 @@ bool XtensaPassConfig::addInstSelector() {
   addPass(createXtensaISelDag(getXtensaTargetMachine(), getOptLevel()));
   return false;
 }
+
+void XtensaPassConfig::addPreEmitPass() { addPass(&BranchRelaxationPassID); }
 
 TargetPassConfig *XtensaTargetMachine::createPassConfig(PassManagerBase &PM) {
   return new XtensaPassConfig(*this, PM);
