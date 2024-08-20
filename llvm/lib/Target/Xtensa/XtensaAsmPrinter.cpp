53892bfeadf59f3878e481138b37e4abdee4572d
Andrei Safronov <safronov@espressif.com>
2024-08-20 19:18:03 +0300

    [Xtensa] Add emit constant pool option.

--- llvm/lib/Target/Xtensa/XtensaAsmPrinter.cpp
+++ llvm/lib/Target/Xtensa/XtensaAsmPrinter.cpp
@@ -150,6 +150,7 @@ void XtensaAsmPrinter::emitConstantPool() {
   auto *TS =
       static_cast<XtensaTargetStreamer *>(OutStreamer->getTargetStreamer());
   MCSection *CS = getObjFileLowering().SectionForGlobal(&F, TM);
+  TS->setTextSectionLiterals();
   TS->startLiteralSection(CS);
 
   int CPIdx = 0;
