497d2a20ee4d05054102e88fdbe40790097300db
Andrei Safronov <safronov@espressif.com>
2024-08-20 12:46:48 +0300

    [Xtensa] Implement esp32 psram cache fixes.

--- llvm/lib/Target/Xtensa/XtensaTargetMachine.cpp
+++ llvm/lib/Target/Xtensa/XtensaTargetMachine.cpp
@@ -143,6 +143,7 @@ void XtensaPassConfig::addPreRegAlloc() {
 }
 
 void XtensaPassConfig::addPreEmitPass() {
+  addPass(createXtensaPSRAMCacheFixPass());
   addPass(createXtensaSizeReductionPass());
   addPass(createXtensaFixupHwLoops());
   addPass(&BranchRelaxationPassID);
