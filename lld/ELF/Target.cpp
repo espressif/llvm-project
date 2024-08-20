90bd39f7bfd8b231d34756443e09eb5fa65d2059
Andrei Safronov <safronov@espressif.com>
2024-08-20 17:44:21 +0300

    [Xtensa] Add LLD linker support

--- lld/ELF/Target.cpp
+++ lld/ELF/Target.cpp
@@ -91,6 +91,8 @@ TargetInfo *elf::getTarget() {
     return getSystemZTargetInfo();
   case EM_X86_64:
     return getX86_64TargetInfo();
+  case EM_XTENSA:
+    return getXtensaTargetInfo();
   default:
     fatal("unsupported e_machine value: " + Twine(config->emachine));
   }
