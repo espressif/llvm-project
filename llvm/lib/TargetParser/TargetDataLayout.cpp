a286e93eb158d625f192f1ffaed2af9ba0e337ee
Andrei Safronov <safronov@espressif.com>
2024-09-19 02:18:38 +0300

    [Xtensa] Fix i8/i16 ABI alignment.
    Set 32-bit alignmnet for i8 and i16 types.

--- llvm/lib/TargetParser/TargetDataLayout.cpp
+++ llvm/lib/TargetParser/TargetDataLayout.cpp
@@ -596,7 +596,7 @@ std::string Triple::computeDataLayout(StringRef ABIName) const {
   case Triple::xcore:
       return "e-m:e-p:32:32-i8:8:32-i16:16:32-i64:64-n32";
   case Triple::xtensa:
-      return "e-m:e-p:32:32-i8:8:32-i16:16:32-i64:64-i128:128-n32";
+      return "e-m:e-p:32:32-i64:64-i128:128-n32";
   case Triple::nvptx:
   case Triple::nvptx64:
     return computeNVPTXDataLayout(*this, ABIName);
