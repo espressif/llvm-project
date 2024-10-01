2035eca1fadcbe9bf04e3ac17b5a20a4723ea606
Andrei Safronov <safronov@espressif.com>
2024-10-01 16:23:58 +0300

    [Xtensa] Add vector conversion builtins
    Intrinsics: __builtin_xtensa_ae_int32x2 and builtin_xtensa_int32
    are convenience functions for easy integer-vector and vector-vector
    conversions that conform to Xtensa coding style.
    Xtensa C dialect allows for implicit conversion between wider and
    narrower vector (via shuffle) and between integer and any vector (via
    broadcast). Standard Clang vectors do not support this, so these
    functions provide a handicap for better portability.

--- clang/lib/CodeGen/CodeGenFunction.h
+++ clang/lib/CodeGen/CodeGenFunction.h
@@ -4806,6 +4806,9 @@ public:
   llvm::Value *EmitXtensaBuiltinExpr(unsigned BuiltinID, const CallExpr *E,
                                      ReturnValueSlot ReturnValue,
                                      llvm::Triple::ArchType Arch);
+  llvm::Value *EmitXtensaConversionExpr(unsigned BuiltinID, const CallExpr *E,
+                                        ReturnValueSlot ReturnValue,
+                                        llvm::Triple::ArchType Arch);
   //===--------------------------------------------------------------------===//
   //                           Expression Emission
   //===--------------------------------------------------------------------===//
