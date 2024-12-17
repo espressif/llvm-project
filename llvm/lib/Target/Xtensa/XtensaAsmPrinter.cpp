6beb85ffc4dac38a021dc0b1bba49b0970854b63
Andrei Safronov <safronov@espressif.com>
2024-12-18 00:38:05 +0300

    [Xtensa] Fix emitting literals section.

--- llvm/lib/Target/Xtensa/XtensaAsmPrinter.cpp
+++ llvm/lib/Target/Xtensa/XtensaAsmPrinter.cpp
@@ -185,7 +185,7 @@ void XtensaAsmPrinter::emitConstantPool() {
   auto *TS =
       static_cast<XtensaTargetStreamer *>(OutStreamer->getTargetStreamer());
   MCSection *CS = getObjFileLowering().SectionForGlobal(&F, TM);
-  TS->setTextSectionLiterals();
+ // TS->setTextSectionLiterals();
   TS->startLiteralSection(CS);
 
   int CPIdx = 0;
