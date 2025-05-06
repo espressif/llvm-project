06d605de5883cf39ceb7a5efc69edde2c60e3e76
chenqian <chen.qian@espressif.com>
2025-05-06 17:47:28 +0800

    [Pass] RISCVDotprodSplitter Passed

--- llvm/lib/Target/RISCV/RISCVTargetMachine.cpp
+++ llvm/lib/Target/RISCV/RISCVTargetMachine.cpp
@@ -16,6 +16,7 @@
 #include "RISCVCustomLICM.h"
 #include "RISCVEsp32P4MemIntrin.h"
 #include "RISCVIntLoopUnrollAndRemainder.h"
+#include "RISCVDotprodSplitter.h"
 #include "RISCVLoopUnrollAndRemainder.h"
 #include "RISCVMachineFunctionInfo.h"
 #include "RISCVTargetObjectFile.h"
@@ -679,6 +680,10 @@ void RISCVTargetMachine::registerPassBuilderCallbacks(PassBuilder &PB) {
         }
         if (Name == "riscv-int-loop-unroll-and-remainder") {
           FPM.addPass(RISCVIntLoopUnrollAndRemainderPass());
+          return true;
+        }
+        if (Name == "riscv-dotprod-splitter") {
+          FPM.addPass(RISCVDotprodSplitterPass());
           return true;
         }
         return false;
