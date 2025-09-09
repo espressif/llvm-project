6402c34b089b53cb7c40bd9da51fcdee58aca8e9
Andrei Safronov <safronov@espressif.com>
2025-09-10 02:34:36 +0300

    [Xtensa] Implemented builtins base functionality for Xtensa.

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -184,6 +184,7 @@ class SemaSwift;
 class SemaSystemZ;
 class SemaWasm;
 class SemaX86;
+class SemaXtensa;
 class StandardConversionSequence;
 class TemplateArgument;
 class TemplateArgumentLoc;
@@ -1580,6 +1581,11 @@ public:
     return *X86Ptr;
   }
 
+  SemaXtensa &Xtensa() {
+    assert(XtensaPtr);
+    return *XtensaPtr;
+  }
+
   /// Source of additional semantic information.
   IntrusiveRefCntPtr<ExternalSemaSource> ExternalSource;
 
@@ -1639,6 +1645,7 @@ private:
   std::unique_ptr<SemaSystemZ> SystemZPtr;
   std::unique_ptr<SemaWasm> WasmPtr;
   std::unique_ptr<SemaX86> X86Ptr;
+  std::unique_ptr<SemaXtensa> XtensaPtr;
 
   ///@}
 
