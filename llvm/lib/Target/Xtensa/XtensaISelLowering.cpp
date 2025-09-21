31c5fce81643607b7fe5336b4c5dd6f4ed9db3e2
Andrei Safronov <safronov@espressif.com>
2025-09-21 14:46:38 +0300

    [Xtensa] Implement ESP32 S3 DSP instructions.

--- llvm/lib/Target/Xtensa/XtensaISelLowering.cpp
+++ llvm/lib/Target/Xtensa/XtensaISelLowering.cpp
@@ -2202,6 +2202,6 @@ MachineBasicBlock *XtensaTargetLowering::EmitInstrWithCustomInserter(
     return MBB;
   }
   default:
-    llvm_unreachable("Unexpected instr type to insert");
+    return EmitDSPInstrWithCustomInserter(MI, MBB, TII, MF, MRI, DL);
   }
 }
