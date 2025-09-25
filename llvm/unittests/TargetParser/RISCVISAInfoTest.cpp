a051d9b55123ac8b9d6317bd61983f69e347022d
Stefan Stipanovic <stefan.stipanovic@espressif.com>
2025-09-25 08:31:26 +0200

    [ESP32H4] Implement support for custom dsp instructions.

--- llvm/unittests/TargetParser/RISCVISAInfoTest.cpp
+++ llvm/unittests/TargetParser/RISCVISAInfoTest.cpp
@@ -1328,6 +1328,7 @@ R"(All available -march extensions for RISC-V
     xcvmac               1.0
     xcvmem               1.0
     xcvsimd              1.0
+    xespdsp              1.0
     xesppie              1.0
     xmipscbop            1.0
     xmipscmov            1.0
