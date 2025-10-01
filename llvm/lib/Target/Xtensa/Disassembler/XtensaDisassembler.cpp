1f13bc813b24e63fa6708a7b66f32b94269e5af2
Andrei Safronov <safronov@espressif.com>
2025-10-02 01:27:35 +0300

    [Xtensa] Support for asm underscore prefix.

--- llvm/lib/Target/Xtensa/Disassembler/XtensaDisassembler.cpp
+++ llvm/lib/Target/Xtensa/Disassembler/XtensaDisassembler.cpp
@@ -480,6 +480,14 @@ static DecodeStatus decodeShimm1_31Operand(MCInst &Inst, uint64_t Imm,
   return MCDisassembler::Success;
 }
 
+static DecodeStatus decodeShimm0_31Operand(MCInst &Inst, uint64_t Imm,
+                                           int64_t Address,
+                                           const void *Decoder) {
+  assert(isUInt<5>(Imm) && "Invalid immediate");
+  Inst.addOperand(MCOperand::createImm(32 - Imm));
+  return MCDisassembler::Success;
+}
+
 static int64_t TableB4const[16] = {-1, 1,  2,  3,  4,  5,  6,   7,
                                    8,  10, 12, 16, 32, 64, 128, 256};
 static DecodeStatus decodeB4constOperand(MCInst &Inst, uint64_t Imm,
