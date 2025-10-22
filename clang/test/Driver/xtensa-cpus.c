679324b8ac30eae54872cc9e3f0a9a6f9ca6e4d0
Andrei Safronov <safronov@espressif.com>
2025-10-22 12:36:58 +0300

    [Xtensa] Fix highpriinterrupts-level* feature.

--- clang/test/Driver/xtensa-cpus.c
+++ clang/test/Driver/xtensa-cpus.c
@@ -3,7 +3,7 @@
 // RUN: %clang -target xtensa -### -c %s 2>&1 -mcpu=esp8266 | FileCheck -check-prefix=MCPU-ESP8266 %s
 // MCPU-ESP8266: "-target-cpu" "esp8266"
 // MCPU-ESP8266: "-target-feature" "+density" "-target-feature" "+nsa" "-target-feature" "+mul32" "-target-feature" "+mul16" "-target-feature" "+extendedl32r"
-// MCPU-ESP8266: "-target-feature" "+debug" "-target-feature" "+exception" "-target-feature" "+highpriinterrupts" "-target-feature" "+highpriinterruptslevel3"
+// MCPU-ESP8266: "-target-feature" "+debug" "-target-feature" "+exception" "-target-feature" "+highpriinterrupts" "-target-feature" "+highpriinterrupts-level3"
 // MCPU-ESP8266: "-target-feature" "+interrupt" "-target-feature" "+rvector" "-target-feature" "+timers1" "-target-feature" "+prid"
 // MCPU-ESP8266: "-target-feature" "+regprotect"
 
@@ -14,7 +14,7 @@
 // MCPU-ESP32: "-target-feature" "+minmax" "-target-feature" "+mac16" "-target-feature" "+mul32" "-target-feature" "+mul32high"
 // MCPU-ESP32: "-target-feature" "+div32" "-target-feature" "+mul16" "-target-feature" "+dfpaccel" "-target-feature" "+s32c1i"
 // MCPU-ESP32: "-target-feature" "+threadptr" "-target-feature" "+dcache" "-target-feature" "+debug" "-target-feature" "+exception"
-// MCPU-ESP32: "-target-feature" "+highpriinterrupts" "-target-feature" "+highpriinterruptslevel7" "-target-feature" "+coprocessor"
+// MCPU-ESP32: "-target-feature" "+highpriinterrupts" "-target-feature" "+highpriinterrupts-level7" "-target-feature" "+coprocessor"
 // MCPU-ESP32: "-target-feature" "+interrupt" "-target-feature" "+rvector" "-target-feature" "+timers3" "-target-feature" "+prid"
 // MCPU-ESP32: "-target-feature" "+regprotect" "-target-feature" "+miscsr"
 
@@ -23,7 +23,7 @@
 // MCPU-ESP32S2: "-target-feature" "+density" "-target-feature" "+windowed" "-target-feature" "+sext" "-target-feature" "+nsa"
 // MCPU-ESP32S2: "-target-feature" "+clamps" "-target-feature" "+minmax" "-target-feature" "+mul32" "-target-feature" "+mul32high"
 // MCPU-ESP32S2: "-target-feature" "+div32" "-target-feature" "+mul16" "-target-feature" "+threadptr" "-target-feature" "+dcache"
-// MCPU-ESP32S2: "-target-feature" "+debug" "-target-feature" "+exception" "-target-feature" "+highpriinterrupts" "-target-feature" "+highpriinterruptslevel7"
+// MCPU-ESP32S2: "-target-feature" "+debug" "-target-feature" "+exception" "-target-feature" "+highpriinterrupts" "-target-feature" "+highpriinterrupts-level7"
 // MCPU-ESP32S2: "-target-feature" "+coprocessor" "-target-feature" "+interrupt" "-target-feature" "+rvector" "-target-feature" "+timers3"
 // MCPU-ESP32S2: "-target-feature" "+prid" "-target-feature" "+regprotect" "-target-feature" "+miscsr" "-target-feature" "+esp32s2ops"
 
@@ -34,6 +34,6 @@
 // MCPU-ESP32S3: "-target-feature" "+minmax" "-target-feature" "+mac16" "-target-feature" "+mul32" "-target-feature" "+mul32high"
 // MCPU-ESP32S3: "-target-feature" "+div32" "-target-feature" "+mul16" "-target-feature" "+s32c1i"
 // MCPU-ESP32S3: "-target-feature" "+threadptr" "-target-feature" "+dcache" "-target-feature" "+debug" "-target-feature" "+exception"
-// MCPU-ESP32S3: "-target-feature" "+highpriinterrupts" "-target-feature" "+highpriinterruptslevel7" "-target-feature" "+coprocessor"
+// MCPU-ESP32S3: "-target-feature" "+highpriinterrupts" "-target-feature" "+highpriinterrupts-level7" "-target-feature" "+coprocessor"
 // MCPU-ESP32S3: "-target-feature" "+interrupt" "-target-feature" "+rvector" "-target-feature" "+timers3" "-target-feature" "+prid"
 // MCPU-ESP32S3: "-target-feature" "+regprotect" "-target-feature" "+miscsr" "-target-feature" "+esp32s3ops"
